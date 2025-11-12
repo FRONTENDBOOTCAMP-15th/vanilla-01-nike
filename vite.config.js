bc4eb1abd8dbcbdb31cd837baf8985b0c60a6bb2
Lee HaYeong <dlgkdud4759@naver.com>
2025-11-12 09:28:58 +0900

    [🌟 Feat]: 테일윈드 적용 및 폴더 구조 세팅

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   build: {
     rollupOptions: {
       input: {
         // 메인 페이지
         index: 'index.html',
-
-        // 에러 페이지
-        // error: "src/pages/error.html",
       },
     },
   },
