5182753320d44ec3b311050b36726b01c4034fac
soo1991 <soo1991@gmail.com>
2025-11-17 17:49:41 +0900

    🌟 Feat: 장바구니 페이지- 장바구니 목록 컴포넌트(수정)

--- src/components/cart/CartTotal.ts
+++ src/components/cart/CartTotal.ts
@@ -5,52 +5,49 @@ class Cart extends HTMLElement {
 
   render() {
     this.innerHTML = `
-    <div class="wrap bg-white mx-auto w-[360px] pl-6 pr-6">
+    <div class="wrap bg-white mx-auto w-[360px]">
       <section class="cart">
-        <div class="cartTotal text-center">
-          <h1 class="text-2xl">장바구니</h1>
-          <div class="relative">
-            <span class="cartCount text-gray-500">1개의 제품</span>
-            <span
-              class="after:absolute after:left-1/2 after:top-0 after:h-full after:w-px after:bg-gray-400"
-            ></span>
-            <span class="cartPrice">189,000원</span>
+        <div class="cartTotal mt-14 mb-[30px] text-center">
+          <h1 class="text-2xl font-medium leading-9 text-[#111]">장바구니</h1>
+          <div class="relative leading-7">
+            <span class="cartCount text-[#707072]">1개의 제품</span>
+            <span class="absolute left-1/2 top-1.5 w-px h-4 -ml-0.5 bg-[#707070] -translate-x-1/2"></span>
+            <span class="cartPrice ml-3 text-[#111]">189,000원</span>
           </div>
         </div>
-        <!-- 장바구니에 상품이 있는 경우 -->
-        <div class="cartList">
-          <div class="cartDetail flex mt-10">
-            <img class="size-[150px]" src="NikeTest.png" alt="운동화" />
-            <div class="productInfo">
-              <p class="price">189,000원</p>
-              <p class="name">Nike Air Max 270</p>
-              <p class="target text-gray-500">남성 신발</p>
-              <table>
+        <!-- 장바구니에 상품이 있는 경우 / div에 이미지 나중에 추가하기-->
+        <div class="cartList mx-auto w-[328px] pt-10 pb-10 border-t border-t-[#e5e5e5]">
+          <div class="cartDetail flex">
+            <div class="shrink-0 w-[154px] h-[154px] bg-amber-200"></div>
+            <div class="productInfo w-full ml-3 leading-7">
+              <p class="price font-medium">189,000원</p>
+              <p class="name font-medium">나이티 줌 보메로 5</p>
+              <p class="target text-[rgb(112,112,114)]">남성 신발</p>
+              <table class="w-full">
                 <tr>
-                  <td class="text-gray-500">사이즈</td>
-                  <td class="text-gray-500">수량</td>
+                  <td class="text-[#707072]">사이즈</td>
+                  <td class="ledading-[16px] text-[#707072]">수량</td>
                 </tr>
                 <tr>
-                  <td class="text-gray-500">275</td>
-                  <td class="text-gray-500">
-                    <button class="text-gray-500" type="button">-</button>
-                    <span>1</span>
-                    <button class="text-gray-500" type="button">+</button>
+                  <td class="w-1/2 underline text-[#707072]">275</td>
+                  <td class="w-1/2">
+                    <button class="w-[30px] h-[30px] text-[#a6a6a6]" type="button">-</button>
+                    <span class="text-[14px] text-[#707072]">1</span>
+                    <button class="w-[30px] h-[30px] text-[#333]" type="button">+</button>
                   </td>
                 </tr>
               </table>
-              <div class="cartButtons mt-10">
-                <span class="likeButton">♥</span>
-                <button class="deleteButton">삭제</button>
+              <div class="cartButtons flex mt-7.5">
+                <span class="w-6 h-6 bg-amber-200"></span>
+                <button class="deleteButton w-6 h-6 ml-4 bg-amber-200"></button>
               </div>
             </div>
           </div>
-
-          <div class="deliveryInfo mt-10">
-            <p>무료배송</p>
-            <p>
+          <div class="deliveryInfo mt-[17px]">
+            <p class="font-medium leading-7 text-[#111]">무료배송</p>
+            <p class="leading-7">
               도착 예정일: 7월 27일 (토) 배송지역:
-              <a href="#" class="underline">04528</a>
+              <a href="#" class="ml-[5px] font-medium leading-7 text-[#111] underline">04528</a>
             </p>
           </div>
         </div>
