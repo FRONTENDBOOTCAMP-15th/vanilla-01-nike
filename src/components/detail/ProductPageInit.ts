a822680b479c8d60ee36cead9862b2dda594df45
Lee HaYeong <dlgkdud4759@naver.com>
2025-11-21 17:36:13 +0900

    🌟 feat: ProductButtonBottom 추가

--- src/components/detail/ProductPageInit.ts
+++ src/components/detail/ProductPageInit.ts
@@ -1,9 +1,13 @@
 import { getProductIdFromUrl } from '../../utils/Id';
-import { ProductButtonTop } from './Button';
+import { ProductButtonTop, ProductButtonBottom } from './Button';
 
 window.addEventListener('DOMContentLoaded', () => {
   const buttonTop =
     document.querySelector<ProductButtonTop>('product-button-top');
+
+  const buttonBottom = document.querySelector<ProductButtonBottom>(
+    'product-button-bottom',
+  );
 
   // URL 쿼리스트링에서 제품 ID 가져오기
   const productIdStr = getProductIdFromUrl();
@@ -12,5 +16,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // 상품 ID가 존재할 경우 버튼 컴포넌트에 전달
   if (buttonTop && productId) {
     buttonTop.setProductId(productId);
+  }
+  if (buttonBottom && productId) {
+    buttonBottom.setProductId(productId);
   }
 });
